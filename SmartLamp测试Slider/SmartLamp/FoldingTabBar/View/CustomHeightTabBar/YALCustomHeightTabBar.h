2e7edc9e1ba1b51b29f39bcadd630e32650971f9
Aesir <Aesir@Aesir-MacBook-Pro.local>
2016-05-17 15:04:22 +0800

    0517

--- SmartLamp测试Slider/SmartLamp/FoldingTabBar/View/CustomHeightTabBar/YALCustomHeightTabBar.h
+++
@@ -1,9 +0,0 @@
-// For License please refer to LICENSE file in the root of YALAnimatingTabBarController project
-
-#import <UIKit/UIKit.h>
-
-@interface YALCustomHeightTabBar : UITabBar
-
-@property (nonatomic) CGFloat barHeight;
-
-@end
